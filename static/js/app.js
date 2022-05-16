74865d5228fc9254b3aa8f68c88f08492437f7bb
AAVWALSH <aavwalsh@gmail.com>
2022-05-15 22:20:00 -0400

    updating

--- static/js/app.js
+++ static/js/app.js
@@ -47,21 +47,5 @@ d3.selectAll("#filter-btn").on("click", handleClick);
 
 // Build the table when the page loads
 buildTable(tableData);
-<div class="col-md-9">
-            <table class="table table-striped">
-              <thead>
-                <tr>
-                  <th>
-                  <th>Date</th>
-                  <th>City</th>
-                  <th>State</th>
-                  <th>Country</th>
-                  <th>Shape</th>
-                  <th>Duration</th>
-                  <th>Comments</th>
-                  </th>
-                </tr>
-              </thead>
-              <tbody></tbody>
-            </table>
-          </div>
+
+
